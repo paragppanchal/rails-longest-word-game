811245af43a710ed881960e0beb3f5fc0a4ad180
Parag Panchal <parag@relteksys.com.au>
2018-08-06 17:43:34 +1000

    done but more testing needed

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,46 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    no_of_letters = rand(4..10)
+    for i in 0...no_of_letters do
+      @letters << ('a'..'z').to_a.sample
+    end
+  end
+
+  def score
+
+    @word = params[:word]
+    @letters = params[:letters].split(',')
+
+    if !valid_pick?(@letters, @word)
+      @result = "Sorry but #{@word} can't be build out of #{@letters.join(',')}"
+    else
+      url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+      url_response = open(url).read
+      response = JSON.parse(url_response)
+      if( response["found"] == false )
+        @result = "Sorry but #{@word} does not seem to be a valid English word..."
+      else
+        @result = "Congratulations! #{@word} is a valid English word!"
+      end
+    end
+  end
+end
+
+private
+
+def valid_pick?(letters, picked)
+  letters = letters.dup
+  picked.each_char do |c|
+    index = letters.index(c)
+    if index
+      letters.delete_at(index)
+    else
+      return false
+    end
+  end
+  return true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'new', to: 'games#new', as: :new
+  post 'score', to: 'games#score', as: :score
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
